5a62154ebd686ee68cd3a95d89e4395a51354bfa
J.C. Burnworth <jc.burnworth@S05010-MBPR.local>
2019-11-25 19:29:00 -0600

    Initial code for add and edit food form

--- BFT/frmAddEditFood.Designer.cs
+++ BFT/frmAddEditFood.Designer.cs
@@ -28,12 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAddEditFood";
+            this.gbAddFoodItem = new System.Windows.Forms.GroupBox();
+            this.tbFoodName = new System.Windows.Forms.TextBox();
+            this.tbFats = new System.Windows.Forms.TextBox();
+            this.tbProtein = new System.Windows.Forms.TextBox();
+            this.tbCarbs = new System.Windows.Forms.TextBox();
+            this.btnSaveFood = new System.Windows.Forms.Button();
+            this.lblFats = new System.Windows.Forms.Label();
+            this.lblProtein = new System.Windows.Forms.Label();
+            this.lblCarbs = new System.Windows.Forms.Label();
+            this.btnFoodName = new System.Windows.Forms.Label();
+            this.gbEditFoodItems = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.gbAddFoodItem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbAddFoodItem
+            // 
+            this.gbAddFoodItem.Controls.Add(this.tbFoodName);
+            this.gbAddFoodItem.Controls.Add(this.tbFats);
+            this.gbAddFoodItem.Controls.Add(this.tbProtein);
+            this.gbAddFoodItem.Controls.Add(this.tbCarbs);
+            this.gbAddFoodItem.Controls.Add(this.btnSaveFood);
+            this.gbAddFoodItem.Controls.Add(this.lblFats);
+            this.gbAddFoodItem.Controls.Add(this.lblProtein);
+            this.gbAddFoodItem.Controls.Add(this.lblCarbs);
+            this.gbAddFoodItem.Controls.Add(this.btnFoodName);
+            this.gbAddFoodItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbAddFoodItem.Location = new System.Drawing.Point(12, 12);
+            this.gbAddFoodItem.Name = "gbAddFoodItem";
+            this.gbAddFoodItem.Size = new System.Drawing.Size(1450, 478);
+            this.gbAddFoodItem.TabIndex = 0;
+            this.gbAddFoodItem.TabStop = false;
+            this.gbAddFoodItem.Text = "Add Food Item";
+            // 
+            // tbFoodName
+            // 
+            this.tbFoodName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFoodName.Location = new System.Drawing.Point(466, 64);
+            this.tbFoodName.Name = "tbFoodName";
+            this.tbFoodName.Size = new System.Drawing.Size(870, 62);
+            this.tbFoodName.TabIndex = 1;
+            // 
+            // tbFats
+            // 
+            this.tbFats.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFats.Location = new System.Drawing.Point(990, 182);
+            this.tbFats.Name = "tbFats";
+            this.tbFats.Size = new System.Drawing.Size(346, 62);
+            this.tbFats.TabIndex = 5;
+            this.tbFats.Leave += new System.EventHandler(this.tbFats_Leave);
+            // 
+            // tbProtein
+            // 
+            this.tbProtein.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbProtein.Location = new System.Drawing.Point(466, 306);
+            this.tbProtein.Name = "tbProtein";
+            this.tbProtein.Size = new System.Drawing.Size(346, 62);
+            this.tbProtein.TabIndex = 7;
+            this.tbProtein.Leave += new System.EventHandler(this.tbProtein_Leave);
+            // 
+            // tbCarbs
+            // 
+            this.tbCarbs.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCarbs.Location = new System.Drawing.Point(466, 182);
+            this.tbCarbs.Name = "tbCarbs";
+            this.tbCarbs.Size = new System.Drawing.Size(346, 62);
+            this.tbCarbs.TabIndex = 3;
+            this.tbCarbs.Leave += new System.EventHandler(this.tbCarbs_Leave);
+            // 
+            // btnSaveFood
+            // 
+            this.btnSaveFood.Location = new System.Drawing.Point(990, 310);
+            this.btnSaveFood.Name = "btnSaveFood";
+            this.btnSaveFood.Size = new System.Drawing.Size(279, 71);
+            this.btnSaveFood.TabIndex = 8;
+            this.btnSaveFood.Text = "Save Food";
+            this.btnSaveFood.UseVisualStyleBackColor = true;
+            this.btnSaveFood.Click += new System.EventHandler(this.btnSaveFood_Click);
+            // 
+            // lblFats
+            // 
+            this.lblFats.AutoSize = true;
+            this.lblFats.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFats.Location = new System.Drawing.Point(854, 186);
+            this.lblFats.Name = "lblFats";
+            this.lblFats.Size = new System.Drawing.Size(130, 55);
+            this.lblFats.TabIndex = 4;
+            this.lblFats.Text = "Fats:";
+            // 
+            // lblProtein
+            // 
+            this.lblProtein.AutoSize = true;
+            this.lblProtein.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProtein.Location = new System.Drawing.Point(106, 310);
+            this.lblProtein.Name = "lblProtein";
+            this.lblProtein.Size = new System.Drawing.Size(213, 55);
+            this.lblProtein.TabIndex = 6;
+            this.lblProtein.Text = "Proteins:";
+            // 
+            // lblCarbs
+            // 
+            this.lblCarbs.AutoSize = true;
+            this.lblCarbs.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarbs.Location = new System.Drawing.Point(106, 186);
+            this.lblCarbs.Name = "lblCarbs";
+            this.lblCarbs.Size = new System.Drawing.Size(354, 55);
+            this.lblCarbs.TabIndex = 2;
+            this.lblCarbs.Text = "Carbohydrates:";
+            // 
+            // btnFoodName
+            // 
+            this.btnFoodName.AutoSize = true;
+            this.btnFoodName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoodName.Location = new System.Drawing.Point(106, 68);
+            this.btnFoodName.Name = "btnFoodName";
+            this.btnFoodName.Size = new System.Drawing.Size(289, 55);
+            this.btnFoodName.TabIndex = 0;
+            this.btnFoodName.Text = "Food Name:";
+            // 
+            // gbEditFoodItems
+            // 
+            this.gbEditFoodItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbEditFoodItems.Location = new System.Drawing.Point(12, 522);
+            this.gbEditFoodItems.Name = "gbEditFoodItems";
+            this.gbEditFoodItems.Size = new System.Drawing.Size(1450, 598);
+            this.gbEditFoodItems.TabIndex = 1;
+            this.gbEditFoodItems.TabStop = false;
+            this.gbEditFoodItems.Text = "Edit Food Items";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(781, 1167);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(225, 50);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Location = new System.Drawing.Point(379, 1167);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(225, 50);
+            this.btnMainMenu.TabIndex = 3;
+            this.btnMainMenu.Text = "Return to Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.btnMainMenu_Click);
+            // 
+            // frmAddEditFood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1474, 1229);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.gbEditFoodItems);
+            this.Controls.Add(this.gbAddFoodItem);
+            this.MaximumSize = new System.Drawing.Size(1500, 1300);
+            this.Name = "frmAddEditFood";
+            this.Text = "Manage Food Items";
+            this.gbAddFoodItem.ResumeLayout(false);
+            this.gbAddFoodItem.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbAddFoodItem;
+        private System.Windows.Forms.GroupBox gbEditFoodItems;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Label btnFoodName;
+        private System.Windows.Forms.Label lblFats;
+        private System.Windows.Forms.Label lblProtein;
+        private System.Windows.Forms.Label lblCarbs;
+        private System.Windows.Forms.Button btnSaveFood;
+        private System.Windows.Forms.TextBox tbFats;
+        private System.Windows.Forms.TextBox tbProtein;
+        private System.Windows.Forms.TextBox tbCarbs;
+        private System.Windows.Forms.TextBox tbFoodName;
     }
 }
--- BFT/frmAddEditFood.cs
+++ BFT/frmAddEditFood.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace BFT
 {
@@ -15,6 +18,117 @@ namespace BFT
         public frmAddEditFood()
         {
             InitializeComponent();
+            // Need to make sure this is updated to capture global userID variable
+            LoadFoodItemData(1);
+        }
+        
+        ///// Code to add food items to the foods table /////
+        // Evaluate data inputs to ensure numbers or data will not load to db
+        private void tbCarbs_Leave(object sender, EventArgs e)
+        {
+            decimal carbs;
+            
+            if (!decimal.TryParse(tbCarbs.Text, out carbs))
+            {
+                MessageBox.Show("Please enter only numbers");
+                tbCarbs.Text = "";
+
+            }
+        }
+
+        private void tbFats_Leave(object sender, EventArgs e)
+        {
+            decimal fats;
+
+            if (!decimal.TryParse(tbFats.Text, out fats))
+            {
+                MessageBox.Show("Please enter only numbers");
+                tbFats.Text = "";
+            }
+        }
+
+        private void tbProtein_Leave(object sender, EventArgs e)
+        {
+            decimal protein;
+
+            if (!decimal.TryParse(tbProtein.Text, out protein))
+            {
+                MessageBox.Show("Please enter only numbers");
+                tbProtein.Text = "";
+            }
+
+        }
+
+        // Write food input to table
+        private void btnSaveFood_Click(object sender, EventArgs e)
+        {
+            // Capture input values and transform to proper types
+            string foodName = tbFoodName.Text;
+            decimal carbs = decimal.Parse(tbCarbs.Text);
+            decimal fats = decimal.Parse(tbFats.Text);
+            decimal proteins = decimal.Parse(tbProtein.Text);
+
+            // Insert data into foods table
+            // Need to make sure this is updated to capture global userID variable
+            InsertFoodItemData(1, foodName, carbs, fats, proteins);
+
+            // Reset add food fields
+            tbFoodName.Text = "";
+            tbCarbs.Text = "";
+            tbFats.Text = "";
+            tbProtein.Text = "";
+
+            // After table is written to, refresh food items data grid
+            // Need to make sure this is updated to capture global userID variable
+            LoadFoodItemData(1);
+        }
+
+        private void InsertFoodItemData(int userID, string foodName, decimal carbs, decimal fats, decimal proteins)
+        {
+            //Console.WriteLine("User ID: " + userID);
+            //Console.WriteLine("Food Name: " + foodName);
+            //Console.WriteLine("Carbohydrates: " + carbs);
+            //Console.WriteLine("Fats: " + fats);
+            //Console.WriteLine("Proteins: " + proteins);
+
+            // Setup connection
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\BFT_DB.mdf;Integrated Security=True");
+
+            // Setup SQL commands to check if user already exists
+            SqlCommand cmd = new SqlCommand
+            {
+                CommandType = CommandType.Text,
+                CommandText = "",
+                Connection = conn
+            };
+            SqlDataReader reader;
+
+            // Open connection
+            conn.Open();
+
+            // Execute insert
+            cmd.ExecuteNonQuery();
+
+            // Close connections
+            cmd.Dispose();
+            conn.Close();
+        }
+
+        ///// Code to edit food items to the foods table /////
+        // Load current food items user has entered on form load or change
+        private void LoadFoodItemData(int userID)
+        {
+            Console.WriteLine("Load Data Grid Data");
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
