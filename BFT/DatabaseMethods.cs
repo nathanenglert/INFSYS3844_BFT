bc05ac7b7113a99d39ef537ba115281f35156649
J.C. Burnworth <jc.burnworth@s05010-mbpr.attlocal.net>
2019-12-01 13:54:20 -0600

    Fixing connection string

--- BFT/DatabaseMethods.cs
+++ BFT/DatabaseMethods.cs
@@ -6,8 +6,8 @@ namespace BFT
     {
         public string DBConnectionString()
         {
-            return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\\Mac\Home\Documents\UMSL\INFSYS_6805_C_Sharp\Projects\INFSYS3844_BFT\BFT\BFT_DB.mdf;Integrated Security=True";
-            //return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Directory.GetCurrentDirectory() + @"\BFT_DB.mdf;Integrated Security=True";
+            //return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\\Mac\Home\Documents\UMSL\INFSYS_6805_C_Sharp\Projects\INFSYS3844_BFT\BFT\BFT_DB.mdf;Integrated Security=True";
+            return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Directory.GetCurrentDirectory() + @"\BFT_DB.mdf;Integrated Security=True";
         }
     }
 }
